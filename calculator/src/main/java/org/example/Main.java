5a7ca1c6defe16ba0e50e9b7c4db170fff59dbf5
Aswathanarayanan <aswathavasan@gmail.com>
2023-03-16 11:31:58 +0530

    made functional changes

--- calculator/src/main/java/org/example/Main.java
+++ calculator/src/main/java/org/example/Main.java
@@ -1,58 +1,61 @@
 package org.example;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
+
+    public static int factorial(int n){
+        int p=1;
+        for(int i=1;i<=n;i++)
+            p*=i;
+        return p;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-            char operator;
-            Double number1, number2, result;
+        Double number1,power;
+        int number2;
 
-            // create an object of Scanner class
-            Scanner input = new Scanner(System.in);
+        // create an object of Scanner class
+        System.out.println("Enter your choice");
+        System.out.println("1. Square root function");
+        System.out.println("2. Factorial function");
+        System.out.println("3. Natural logarithm (base)е");
+        System.out.println("4. Power function ");
+        Scanner input = new Scanner(System.in);
+        int operator=input.nextInt();
 
-            // ask users to enter operator
-            System.out.println("Choose an operator: +, -, *, or /");
-            operator = input.next().charAt(0);
+        switch (operator) {
 
-            // ask users to enter numbers
-            System.out.println("Enter first number");
-            number1 = input.nextDouble();
+            case 1:
+                System.out.println("Enter the number");
+                number1=input.nextDouble();
+                System.out.println("Square root of " + number1 +" is " + Math.sqrt(number1) );
+                break;
 
-            System.out.println("Enter second number");
-            number2 = input.nextDouble();
+            case 2:
+                System.out.println("Enter the number");
+                number2=input.nextInt();
+                System.out.println("Factorial of " + number2 +" is " + factorial(number2) );
+                break;
 
-            switch (operator) {
+            case 3:
+                System.out.println("Enter the number");
+                number1=input.nextDouble();
+                System.out.println("Log ( ln(x) ) of " + number1 +" is " + Math.log(number1) );
+                break;
 
-                // performs addition between numbers
-                case '+':
-                    result = number1 + number2;
-                    System.out.println(number1 + " + " + number2 + " = " + result);
-                    break;
+            case 4:
+                System.out.println("Enter the number");
+                number1=input.nextDouble();
+                System.out.println("Enter the power");
+                power=input.nextDouble();
+                System.out.println(number1 +" power " + power + " is " + Math.pow(number1,power) );
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
 
-                // performs subtraction between numbers
-                case '-':
-                    result = number1 - number2;
-                    System.out.println(number1 + " - " + number2 + " = " + result);
-                    break;
-
-                // performs multiplication between numbers
-                case '*':
-                    result = number1 * number2;
-                    System.out.println(number1 + " * " + number2 + " = " + result);
-                    break;
-
-                // performs division between numbers
-                case '/':
-                    result = number1 / number2;
-                    System.out.println(number1 + " / " + number2 + " = " + result);
-                    break;
-
-                default:
-                    System.out.println("Invalid operator!");
-                    break;
-            }
-
-            input.close();
+        input.close();
     }
 }
